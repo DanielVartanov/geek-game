654b6b062ed766acc6498babafd3e0bda20a68be
DanielVartanov <daniel.vartanov@gmail.com>
2010-09-15 18:02:27 +0600

    More extensions for Numeric

--- geometry/lib/numeric_extentsions.rb
+++ geometry/lib/numeric_extentsions.rb
@@ -2,4 +2,18 @@ class Numeric
   def degrees
     self.to_f / 180 * Math::PI
   end
+
+  def sign
+    self.zero? ? 0 : self <=> 0.0
+  end
+
+  def ===(other)
+    (self - other).abs <= Float::EPSILON * 2
+  end
+end
+
+class Float  
+  def zero?
+    self === 0.0
+  end
 end
--- geometry/spec/numeric_extensions_spec.rb
+++ geometry/spec/numeric_extensions_spec.rb
@@ -1,16 +1,92 @@
 __DIR__ = File.dirname(__FILE__)
 require File.join(__DIR__, 'spec_helper')
 
-describe Numeric, "#degreed" do
-  context "when number is Float" do
-    it "should convert radians to degrees" do
-      180.to_f.degrees.should === Math::PI
+describe Numeric do
+  describe "#degrees" do
+    context "when number is Float" do
+      it "should convert radians to degrees" do
+        180.to_f.degrees.should === Math::PI
+      end
+    end
+
+    context "when number is Fixnum" do
+      it "should convert radians to degrees" do
+        90.degrees.should == Math::PI / 2
+      end
     end
   end
 
-  context "when number is Fixnum" do
-    it "should convert radians to degrees" do
-      90.degrees.should == Math::PI / 2
+  describe "#sign" do
+    context "when number is positive" do
+      it "should return 1" do
+        -25.3.sign.should == -1
+      end
+    end
+
+    context "when number is zero" do
+      it "should return 0" do
+        0.sign.should == 0
+      end
+    end
+
+    context "when number is close to zero" do
+      it "should return 0" do
+        Float::EPSILON.sign.should == 0
+      end
+    end
+
+    context "when number is negative" do
+      it "should return -1" do
+        (-Math.sqrt(2)).sign.should == -1
+      end
+    end
+  end
+
+  describe "#zero?" do
+    context "when number is zero" do
+      it "should return true" do
+        0.zero?.should == true
+      end
+    end
+
+    context "when number is close to zero" do
+      it "should return true too" do
+        (- Float::EPSILON).zero?.should == true
+        Float::EPSILON.zero?.should == true
+      end
+    end
+
+    context "when number is far from zero" do
+      it "should return false" do
+        1.zero?.should == false
+        (-Math.sqrt(2)).zero?.should == false
+      end
+    end
+  end
+
+  describe "#===" do
+    context "when two floats are close" do
+      subject { (3 - Float::EPSILON) === 3 }
+
+      it { should be_true }
+    end
+
+    context "when fixnum is close to float" do
+      subject { 3 === (3 + Float::EPSILON) }
+      
+      it { should be_true }
+    end
+
+    context "when two fixnums are equal" do
+      subject { 3 === 3 }
+
+      it { should be_true }
+    end
+
+    context "when two fixnums are not equal" do
+      subject { 2 === 3 }
+
+      it { should be_false }
     end
   end
 end
