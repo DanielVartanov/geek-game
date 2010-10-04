28b91f8cd1a27a6b4312e3752af92e73f9dc9712
DanielVartanov <daniel.vartanov@gmail.com>
2010-10-04 16:15:19 +0600

    AI is a little bit more cleaver now

--- server/ai.rb
+++ server/ai.rb
@@ -1,15 +1,52 @@
 module GeekGame
   module TrackedBotExtensions
-    def aim(target)
-      angle_to_target = Vector(1, 0).signed_angle_with(Vector.by_end_points(self.position, target.position))
-      angle_diff = angle_to_target - gun_angle
+    def vector_to(target)
+      Vector.by_end_points(self.position, target.position)
+    end
+    
+    def angle_to(target)
+      Vector(1, 0).signed_angle_with(vector_to(target))
+    end
 
+    def distance_to(target)
+      position.distance_to(target.position)
+    end
+
+    def stop!
+      motor!(0, 0)
+    end
+
+    def aim(target)
+      angle_diff = angle_to(target) - gun_angle
       gun.rotate(angle_diff)
+    end
+
+    def advance_to(target)
+      movement_vector = Vector(1, 0).rotate(angle).rotate(90.degrees)
+      angle_diff = movement_vector.signed_angle_with(vector_to(target))
+      power = angle_diff.abs < 45.degrees ? angle_diff.abs / 90.degrees : 1
+
+      case angle_diff.sign
+      when -1: motor!(1, 1 - power)
+      when 0: motor!(1, 1)
+      when 1: motor!(1 - power, 1)
+      end
+    end
+
+    def close_enough_to_shoot(target)
+      distance_to(target) < Shell::MAX_RANGE
+    end
+
+    def worth_fire?(target)
+      puts angle_to(target) - gun_angle < 10.degrees and close_enough_to_shoot(target)
+      angle_to(target) - gun_angle < 10.degrees and close_enough_to_shoot(target)
     end
 
     def engage(target)
       aim(target)
-      #fire(target)
+      advance_to(target)
+      stop! if distance_to(target) < 300
+      fire! if worth_fire?(target)
     end
   end
 
