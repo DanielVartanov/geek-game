fa682e7134980445356211ece8192a5e3ab6d443
Vasily Reys <reys.vasily@gmail.com>
2010-10-06 19:50:30 +0600

    Birth time and lifetime

--- lib/game_object.rb
+++ lib/game_object.rb
@@ -1,11 +1,22 @@
 module GeekGame
   class GameObject
+    attr_reader :birth_time
+    
     def initialize
       GeekGame.game_objects << self
+      self.birth_time = Time.now
     end
 
     def die!
       GeekGame.game_objects.delete(self)
     end
+
+    def life_time
+      Time.now - birth_time
+    end    
+
+    protected
+
+    attr_writer :birth_time
   end  
 end
