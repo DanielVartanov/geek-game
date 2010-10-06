6d21389018db03e4b1ef7099e7a73975dc692765
Vasily Reys <reys.vasily@gmail.com>
2010-10-06 19:50:54 +0600

    Color triangle at Shapes

--- client/shapes.rb
+++ client/shapes.rb
@@ -8,9 +8,9 @@ module Shapes
     draw_line(line.point1.to_screen(self), line.point2.to_screen(self), color)
   end
 
-  def triangle(point1, point2, point3)
+  def triangle(point1, point2, point3, color=default_color)
     points = [point1, point2, point3].map { |point| point.to_screen(self) }
-    draw_polygon_s(points, default_color)
+    draw_polygon_s(points, color)
   end
 
   def rectangle(corners, color=default_color)
