9db6a5607ed8e5749456c927013933691943931c
Daniel Vartanov <dan@vartanov.net>
2011-09-11 16:44:46 +0200

    This is how client-server interaction should  look from client side

--- client/client.rb
+++ client/client.rb
@@ -1,45 +1,49 @@
 module GeekGame
-  class Client < Struct.new(:game_objects)
-    def initialize(game_objects)
-      @screen = Rubygame::Screen.new [1920,1000], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
-      @screen.title = "GeekGame"
+  class Client < Struct.new(:host, :port)
+    def initialize(host, port)
+      super(host, port)
 
-      @queue = Rubygame::EventQueue.new
-      @queue.enable_new_style_events
-
-      @clock = Rubygame::Clock.new
-      @clock.target_framerate = 50
-      @clock.calibrate
-      @clock.enable_tick_events
-
-      class << @screen
-        include Shapes
-        include HUD
-
-        def default_color
-          [0xff, 0xff, 00]
-        end
-      end
-
-      # TTF
-
-      Rubygame::TTF.setup
-      point_size = 12
-      @font = Rubygame::TTF.new "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf", point_size
-
-      super(game_objects)
+      setup_screen
+      setup_event_queue
+      setup_clock
+      setup_ttf
     end
 
-    def run
+    def start!
+      @socket = TCPSocket.open host, port
+      
       loop do
-        seconds = @clock.tick.seconds
-        update
-        draw            
+        @clock.tick
+        
+        handle_local_events
+        
+        fresh_data = @socket.gets
+        update_with fresh_data
+        
+        draw_scene
       end
     end
 
-    def update
-      @queue.each do |event|
+    def update_with(data)
+      game_objects = Parse.run data
+      
+      ids_to_destroy = [game_object_images.keys - game_objects.keys]
+      ids_to_destroy.each do |id|
+        game_object_images[id].destroy!
+      end
+      
+      ids_to_create = [game_objects.keys - game_object_images.keys]
+      ids_to_create.each do |id|
+        GameObjectImage.new game_objects[id]
+      end
+
+      game_objects.each do |remote_object|
+        game_object_images[game_object.id].merge!(remote_object)
+      end
+    end
+
+    def handle_local_events
+      @event_queue.each do |event|
         case event
         when Rubygame::Events::QuitRequested
           Rubygame.quit
@@ -48,20 +52,52 @@ module GeekGame
       end
     end
 
-    def draw
+    def draw_scene
       @screen.fill(:black)
 
-      game_objects.bots.each { |bot| Graphics::TrackedBot.new(bot, @screen).draw }
-      game_objects.shells.each { |shell| Graphics::Shell.new(shell, @screen).draw }
-      game_objects.factories.each { |factory| Graphics::Factory.new(factory, @screen).draw }
-      game_objects.rechargers.each { |recharger| Graphics::Recharger.new(recharger, @screen).draw }
+      game_object_images.bots.each { |bot| Graphics::TrackedBot.new(bot, @screen).draw }
+      game_object_images.shells.each { |shell| Graphics::Shell.new(shell, @screen).draw }
+      game_object_images.factories.each { |factory| Graphics::Factory.new(factory, @screen).draw }
+      game_object_images.rechargers.each { |recharger| Graphics::Recharger.new(recharger, @screen).draw }
 
- #     text = "position: (#{@bot.position.x.to_i}, #{@bot.position.y.to_i}) angle: #{(@bot.angle * 180 / Math::PI).to_i} track power: [#{@bot.left_track.power}, #{@bot.right_track.power}]"
-
-      text = "Hello, underworld!"
+      text = "Game has begun"
       @screen.draw_text_info(@font, text)
 
       @screen.flip
     end  
-  end  
+  end
+
+  protected
+
+  def setup_ttf
+    Rubygame::TTF.setup
+    point_size = 12
+    @font = Rubygame::TTF.new "/Library/Fonts/Courier New Bold.ttf", point_size
+  end
+
+  def setup_screen
+    @screen = Rubygame::Screen.new [1280, 800], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+    @screen.title = "GeekGame"
+    
+    class << @screen
+      include Shapes
+      include HUD
+
+      def default_color
+        [0xff, 0xff, 00]
+      end
+    end
+  end
+
+  def setup_clock
+    @clock = Rubygame::Clock.new
+    @clock.target_framerate = 50
+    @clock.calibrate
+    @clock.enable_tick_events
+  end
+
+  def setup_event_queue
+    @event_queue = Rubygame::EventEvent_Queue.new
+    @event_queue.enable_new_style_events
+  end
 end
