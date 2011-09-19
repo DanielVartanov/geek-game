5b0e8c98a30ce2b7c769cc1fb1a187955f073edd
Daniel Vartanov <dan@vartanov.net>
2011-09-19 21:49:48 +0200

    Introducing Network::Server

--- server/network/server.rb
+++ server/network/server.rb
@@ -2,44 +2,46 @@ require "socket"
 require "bson"
 
 module GeekGame
-  class NetworkServer < Struct.new(:timeline, :host, :port)
-    def start!
-      bind_to_port
-      puts "GeekGame server is listening on #{host}:#{port}"
+  module Network
+    class Server < Struct.new(:timeline, :host, :port)
+      def start!
+        bind_to_port
+        puts "GeekGame server is listening on #{host}:#{port}"
+        
+        main_loop
+      end
       
-      main_loop
-    end
-    
 =begin      
-      loop do
-        Thread.start(server.accept) do |client|
-          loop do
-            sleep(0.1)
-            client.puts "ping"
+         loop do
+          Thread.start(server.accept) do |client|
+            loop do
+              sleep(0.1)
+              client.puts "ping"
+            end
           end
         end
-      end
 =end
 
-    protected
+      protected
 
-    attr_accessor :socket
+      attr_accessor :socket
 
-    def bind_to_port
-      self.socket = TCPServer.new(host, port)
-    end
+      def bind_to_port
+        self.socket = TCPServer.new(host, port)
+      end
 
-    def main_loop
-      client = socket.accept
-      puts "Client connected"
+      def main_loop
+        client = socket.accept
+        puts "Client connected"
 
-      loop do
-        sleep(0.1) # do not convert to Rubygame::clock or whatever takes CPU quants
-        objects_hash = GeekGame.game_objects.to_hashes
-        data_to_transmit = BSON.serialize(objects_hash).to_s
-        client.puts data_to_transmit
-        
-        # TODO: add put_byte_array to ruby sockets
+        loop do
+          sleep(0.1) # do not convert to Rubygame::clock or whatever takes CPU quants
+          objects_hash = GeekGame.game_objects.to_hashes
+          data_to_transmit = BSON.serialize(objects_hash).to_s
+          client.puts data_to_transmit
+          
+          # TODO: add #put_byte_array to ruby sockets
+        end
       end
     end
   end
