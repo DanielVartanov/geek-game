560fa5c80edbd1bebd86751113cf93a0dc04e8ad
Daniel Vartanov <dan@vartanov.net>
2016-07-10 12:23:10 +0100

    Fix track_spec

--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -8,46 +8,46 @@ describe Track do
     context "when target power is far from current" do
       context "when target power is greater than current" do
         before :each do
-          track.target_power = Track.power_acceleration * 2
+          track.target_power = PewPewTrack.power_acceleration * 2
           track.update_power(1)
         end
 
         it "should advance power according to acceleration" do
-          track.power.should === Track.power_acceleration
+          track.power.should === PewPewTrack.power_acceleration
         end
       end
 
       context "when target power is lesser than current" do
         before :each do
-          track.target_power = -(Track.power_acceleration * 2)
+          track.target_power = -(PewPewTrack.power_acceleration * 2)
           track.update_power(1)
         end
 
         it "should advance power according to acceleration" do
-          track.power.should === -(Track.power_acceleration)
+          track.power.should === -(PewPewTrack.power_acceleration)
         end
       end
 
       context "when passed time is long too" do
         before :each do
-          track.target_power = 2.5 * Track.power_acceleration
+          track.target_power = 2.5 * PewPewTrack.power_acceleration
           track.update_power(3)
         end
 
         it "should advance power to target one" do
-          track.power.should == 2.5 * Track.power_acceleration
+          track.power.should == 2.5 * PewPewTrack.power_acceleration
         end
       end
     end
 
     context "when target power is close to current" do
       before :each do
-        track.target_power = 0.5 * Track.power_acceleration
+        track.target_power = 0.5 * PewPewTrack.power_acceleration
         track.update_power(3)
       end
 
       it "should advance power to target one" do
-        track.power.should == 0.5 * Track.power_acceleration
+        track.power.should == 0.5 * PewPewTrack.power_acceleration
       end
     end
   end
